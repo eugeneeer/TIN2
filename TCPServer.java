2817b7f0f371bda96d95c9a67e8aec44589c8da0
eugeneeer <eugene.khoroshutin@gmail.com>
2017-04-19 16:03:56 +0200

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -7,22 +7,36 @@ import java.net.*;
 
 public class TCPServer {
     public static void main(String[] args) throws IOException {
-
-//        TODO: Threads, process multiple messages that can be received
-//        ServerSocket ECoSProxy = initializeECoSProxySocket();
-//        String ECoSProxyRequest = "";
-//        while (true) {
-//            Socket connectionSocket = ECoSProxy.accept();
-//            BufferedReader inFromClient =
-//                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-//            ECoSProxyRequest = inFromClient.readLine();
-//            System.out.println("Received: " + ECoSProxyRequest);
-//        }
-//        Socket ECoSSocket = initializeECoSSocket();
-//        String ECoSResponse = executeCommand(ECoSSocket, ECoSProxyRequest);
-//        closeECoSSocket(ECoSSocket);
-//        System.out.println(ECoSResponse);
-        runECoSClientInConsole();
+        System.out.println("README: The listening port is 55557. " +
+                "Configuration has still to be made to separate messages coming from Samsung VR correctly. " +
+                "Now a dummy delimiter '(10)' is used. " +
+                "Hence only commands including '(10)' will be correcntly interpreted. " +
+                "Responses from ECoS are delimited with lines containing '<END>'. " +
+                "Request transformation rules also have to be implemented in method transformRequest()." + "\n");
+        ECoSProxyServer listeningProxyServer = new ECoSProxyServer();
+        Thread t = new Thread(listeningProxyServer);
+        t.start();
+        Socket ECoSSocket = initializeECoSSocket();
+        String VRToProxyRequest = "";
+        String ECoSRequest = "";
+        while (true) {
+            VRToProxyRequest = listeningProxyServer.commandsQueue.poll();
+            if (VRToProxyRequest == null) {
+                continue;
+            }
+            System.out.println("---------------------------");
+            System.out.println("Request Samsung VR -> Proxy Server:");
+            System.out.println(VRToProxyRequest);
+            System.out.println("---------------------------");
+            ECoSRequest = transformRequest(VRToProxyRequest);
+            System.out.println("Request Proxy Server -> ECos Request:");
+            String ECoSResponse = executeCommand(ECoSSocket, ECoSRequest);
+            System.out.println(ECoSRequest);
+            System.out.println("---------------------------");
+            System.out.println("Response ECoS -> Proxy Server:");
+            System.out.println(ECoSResponse);
+            System.out.println("---------------------------");
+        }
     }
 
     public static String readURL() throws IOException {
@@ -32,11 +46,12 @@ public class TCPServer {
         return serverAddress;
     }
 
-    public static int readPort() throws IOException {
+    public static int readListeningPort() throws IOException {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter Port Number:");
+        System.out.println("Enter Listening Port Number:");
         String portNumberInput = inFromUser.readLine();
         int portNumber = Integer.parseInt(portNumberInput);
+        System.out.println(portNumber);
         return portNumber;
     }
 
@@ -48,7 +63,7 @@ public class TCPServer {
     }
 
     public static void runECoSClientInConsole() throws IOException {
-        Socket clientSocket = new Socket("XXX", XXXX);
+        Socket clientSocket = new Socket("bmec.mine.nu", 15471);
         String ECoSRequest = "";
         while (!ECoSRequest.equals("close")) {
             ECoSRequest = readRequest();
@@ -62,8 +77,32 @@ public class TCPServer {
         closeECoSSocket(clientSocket);
     }
 
+    public static void runECoSClientInFile() throws IOException {
+        Socket clientSocket = initializeECoSSocket();
+        String ECoSRequest = "";
+        PrintWriter logFile = new PrintWriter("ECoS_Log.txt");
+        while (!ECoSRequest.equals("close")) {
+            ECoSRequest = readRequest();
+            logFile.append("ECoS Request:" + '\n');
+            logFile.append(ECoSRequest + '\n');
+            logFile.append("-------------------------------" + '\n');
+            logFile.flush();
+            if (ECoSRequest.equals("close")) {
+                break;
+            }
+            String ECoSResponse = executeCommand(clientSocket, ECoSRequest);
+            logFile.append("ECoS Response:" + '\n');
+            logFile.append(ECoSResponse);
+            logFile.append("-------------------------------" + '\n');
+            logFile.flush();
+        }
+        closeECoSSocket(clientSocket);
+        logFile.append("Connection closed");
+        logFile.close();
+    }
+
     public static Socket initializeECoSSocket() throws IOException {
-        Socket clientSocket = new Socket("XXX", XXXX);
+        Socket clientSocket = new Socket("bmec.mine.nu", 15471);
         System.out.println("Connection established");
         return clientSocket;
     }
@@ -88,12 +127,8 @@ public class TCPServer {
         return output.toString();
     }
 
-    public static ServerSocket initializeECoSProxySocket() throws IOException {
-        ServerSocket welcomeSocket = new ServerSocket();
-        System.out.println("Allocated Port:" + welcomeSocket.getLocalPort());
-        return welcomeSocket;
+    /* TODO implement transformation rules*/
+    public static String transformRequest(String request) {
+        return request;
     }
 }
-
-
-
